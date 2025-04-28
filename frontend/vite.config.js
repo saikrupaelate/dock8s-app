2c49b85bab2dc56780759005d89c4de965a509b3
saikrupaelate <elatesaikrupa@gmail.com>
2025-04-27 20:27:50 -0400

    front end config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: process.env.FRONTEND_PORT,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // For local development
+        target: 'http://'+ process.env.BACKEND_IP+':' +process.env.BACKEND_PORT,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
